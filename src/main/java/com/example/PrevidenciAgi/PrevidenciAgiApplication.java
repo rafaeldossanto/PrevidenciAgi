719d501b418eac1acd4f9501b5e2f06e8bdb62e0
rafaeldossantos <rafael.santoss2802@gmail.com>
2025-09-17 08:23:09 -0300

    Initial commit

---
+++ src/main/java/com/example/PrevidenciAgi/PrevidenciAgiApplication.java
@@ -0,0 +1,13 @@
+package com.example.PrevidenciAgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrevidenciAgiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrevidenciAgiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PrevidenciAgi/PrevidenciAgiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PrevidenciAgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrevidenciAgiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

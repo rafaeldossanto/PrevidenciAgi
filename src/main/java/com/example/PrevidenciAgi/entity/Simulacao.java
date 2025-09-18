2522090d1a44bf5ca7d1548b48472266eb87f90f
GU5T4V00 <142633768+GU5T4V00@users.noreply.github.com>
2025-09-18 11:26:32 -0300

    Update Simulacao.java

--- src/main/java/com/example/PrevidenciAgi/entity/Simulacao.java
+++ src/main/java/com/example/PrevidenciAgi/entity/Simulacao.java
@@ -1,7 +1,27 @@
 package com.example.PrevidenciAgi.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data //(Lombok) -> gera getters, setters, toString, equals e hashCode
+@NoArgsConstructor // (Lombok) -> gera construtor
+@AllArgsConstructor // (Lombok) -> gera construtor
+@Table(name="Simulação") // nome da tabela no banco
 public class Simulacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto incremento
+    private Long id;
+    private String descricao;
+    private String responsavel;
+    private String objetivo;
+    private String resultado; // Resultado
+
+    //Construtor vazio (obrigatório para o JPA)
 }

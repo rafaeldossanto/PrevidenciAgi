2dd72de7a7d77165e622e8efb0e9c7145ac7f051
GU5T4V00 <142633768+GU5T4V00@users.noreply.github.com>
2025-09-18 11:42:23 -0300

    Update Cliente.java

--- src/main/java/com/example/PrevidenciAgi/entity/Cliente.java
+++ src/main/java/com/example/PrevidenciAgi/entity/Cliente.java
@@ -8,6 +8,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.persistence.OneToMany;
 
 @Entity
 @Data
@@ -20,4 +21,11 @@ public class Cliente {
 
     @NotNull
     private String cpf, nome, genero, email, senha;
+
+    // Campo de relacionamento com a entidade Simulacao
+    // @OneToMany - Indica que um cliente pode ter várias simulações (relação 1:N)
+    // mappedBy = "cliente" - Informa ao JPA que o mapeamento é feito pelo campo "cliente" na classe Simulacao, evitando a criação de uma tabela intermediária
+    // cascade = CascadeType.ALL - Propaga operações (salvar, atualizar, deletar) para as simulações associadas. Por exemplo, se um Cliente for deletado, todas as suas simulações também serão.
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+    private List<Simulacao> simulacoes;
 }

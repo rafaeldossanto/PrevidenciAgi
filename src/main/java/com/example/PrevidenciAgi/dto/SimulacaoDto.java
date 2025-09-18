1b8af3984526de856d76563c6e959523fd1ae216
Gustavo Henrique dos Anjos <142633768+GU5T4V00@users.noreply.github.com>
2025-09-18 20:12:15 -0300

    Update SimulacaoDto.java

--- src/main/java/com/example/PrevidenciAgi/dto/SimulacaoDto.java
+++ src/main/java/com/example/PrevidenciAgi/dto/SimulacaoDto.java
@@ -1,16 +1,21 @@
 package com.example.PrevidenciAgi.dto;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Data // Adicionado para gerar getters e setters
 public class SimulacaoDto {
-    @NotNull(message = "Valor mensal é obrigatório!")
-    private double valorMensal;
 
-    @NotNull(message = "Data Inicial é obrigatória!")
+    @NotNull(message = "O valor mensal não pode ser nulo.")
+    @Positive(message = "O valor mensal deve ser um número positivo.")
+    private Double valorMensal; // Alterado para Double para ser compatível com @NotNull
+
+    @NotNull(message = "A data inicial não pode ser nula.")
     private LocalDateTime dataInicial;
 
-    @NotNull(message = "Data aposentadoria é obrigatório!")
+    @NotNull(message = "A data de aposentadoria não pode ser nula.")
     private LocalDateTime dataAposentar;
 }

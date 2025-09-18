f74964f41582c236d3b00b94ef5f10571f68e7ee
GU5T4V00 <142633768+GU5T4V00@users.noreply.github.com>
2025-09-18 11:40:56 -0300

    Update Simulacao.java

--- src/main/java/com/example/PrevidenciAgi/entity/Simulacao.java
+++ src/main/java/com/example/PrevidenciAgi/entity/Simulacao.java
@@ -1,27 +1,36 @@
 package com.example.PrevidenciAgi.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Entity
-@Data //(Lombok) -> gera getters, setters, toString, equals e hashCode
-@NoArgsConstructor // (Lombok) -> gera construtor
-@AllArgsConstructor // (Lombok) -> gera construtor
+@Data // (Lombok) - Gera automaticamente os getters, setters, toString, equals e hashCode
+@NoArgsConstructor // (Lombok) - Cria um construtor sem argumentos, obrigatório pelo JPA
+@AllArgsConstructor // (Lombok) - Cria um construtor com todos os argumentos
+@Table(name="Simulacao") // Define o nome da tabela no banco de dados
 public class Simulacao {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto incremento
-    private Long idSimulacao;
 
-    private double valorMensal;
-    private LocalDateTime dataInicial;
-    private LocalDateTime dataAposentar;
+    @Id // Marca este campo como a chave primária da tabela
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Configura o banco de dados para gerar automaticamente o ID (auto-incremento)
+    private Long id;
 
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "cliente_id", referencedColumnName = "id")
+    // Atributos da simulação
+    private String descricao;
+    private String objetivo;
+    private String resultado; // Armazena o resultado da simulação
+
+    // Campo de relacionamento com a entidade Cliente
+    // @ManyToOne indica que muitas simulações podem pertencer a um único cliente (relação N:1)
+    @ManyToOne
+    // @JoinColumn(name = "cliente_id") - Cria uma coluna na tabela "simulacao" chamada "cliente_id", que será a chave estrangeira (foreign key)
+    @JoinColumn(name = "cliente_id")
     private Cliente cliente;
-
 }

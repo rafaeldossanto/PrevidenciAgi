94fbcf84081b559ca4967b6b98193cc3884bcc10
dev05 <hhpassarela@gmail.com>
2025-09-18 09:23:27 -0300

    Criação da tabela Cliente contendo: Id,cpf,nome,genero,email,senha.

--- src/main/java/com/example/PrevidenciAgi/entity/Cliente.java
+++ src/main/java/com/example/PrevidenciAgi/entity/Cliente.java
@@ -1,7 +1,23 @@
 package com.example.PrevidenciAgi.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String cpf, nome, genero, email, senha;
 }

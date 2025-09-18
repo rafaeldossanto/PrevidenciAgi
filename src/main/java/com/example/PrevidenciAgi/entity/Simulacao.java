7bb9a301b3b8d089bcdeb62b4c29cbc7fba3e4a4
dev05 <hhpassarela@gmail.com>
2025-09-18 11:39:06 -0300

    Foi corrijido a simulacao e linkada a buscar por id ligada ao cliente.

--- src/main/java/com/example/PrevidenciAgi/entity/Simulacao.java
+++ src/main/java/com/example/PrevidenciAgi/entity/Simulacao.java
@@ -1,27 +1,27 @@
 package com.example.PrevidenciAgi.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Data //(Lombok) -> gera getters, setters, toString, equals e hashCode
 @NoArgsConstructor // (Lombok) -> gera construtor
 @AllArgsConstructor // (Lombok) -> gera construtor
-@Table(name="Simulação") // nome da tabela no banco
 public class Simulacao {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto incremento
-    private Long id;
-    private String descricao;
-    private String responsavel;
-    private String objetivo;
-    private String resultado; // Resultado
+    private Long idSimulacao;
 
-    //Construtor vazio (obrigatório para o JPA)
+    private double valorMensal;
+    private LocalDateTime dataInicial;
+    private LocalDateTime dataAposentar;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "cliente_id", referencedColumnName = "id")
+    private Cliente cliente;
+
 }

8b426ba309f792645d8dfc5c187467d6c75a6ce3
Matheus <m.vencio@yahoo.com.br>
2025-09-18 10:53:06 -0300

    Criei o modelo de aposentadoria (.java) com todos os atributos listados no MER.

--- src/main/java/com/example/PrevidenciAgi/entity/Aposentadoria.java
+++ src/main/java/com/example/PrevidenciAgi/entity/Aposentadoria.java
@@ -2,6 +2,25 @@ package com.example.PrevidenciAgi.entity;
 
 import jakarta.persistence.Entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 @Entity
+@Table(name = "tbAposentadoria") // nome da tabela no banco
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Aposentadoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAposentadoria;
+
+    @Column(nullable = false)
+    private String tipoAposentadoria;   //progressiva ou regressiva
+
+    @Column(nullable = false)
+    private String dataAposentar;       // Data que ele quer se aposentar
+
+    private String dataContratada;      // Data em que contratou o plano
 }

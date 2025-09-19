a6e3f09dac0c182d23c2542b475459315edcb2ad
rafaeldossantos <rafael.santoss2802@gmail.com>
2025-09-19 10:27:06 -0300

    criacao do metodo para atualizar email e senha no banco de dados de algum cliente.

--- src/main/java/com/example/PrevidenciAgi/service/ClienteService.java
+++ src/main/java/com/example/PrevidenciAgi/service/ClienteService.java
@@ -3,8 +3,12 @@ package com.example.PrevidenciAgi.service;
 import com.example.PrevidenciAgi.dto.ClienteDto;
 import com.example.PrevidenciAgi.entity.Cliente;
 import com.example.PrevidenciAgi.repository.ClienteRepository;
+import jakarta.persistence.EntityExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+import java.util.Optional;
 
 
 @Service
@@ -26,5 +30,29 @@ public class ClienteService {
             throw new IllegalArgumentException("Cliente não encontrado.");
         }
         clienteRepository.deleteById(id);
+    }
+
+    public String atualizarDados(Long id, String dado, String dadoNovo) {
+        Cliente cliente = clienteRepository.findById(id)
+                .orElseThrow(() -> new EntityExistsException("Cliente com esse Id nao encontrado."));
+
+        String dadoAntigo;
+
+        switch (dado.toLowerCase()) {
+            case "email":
+                dadoAntigo = cliente.getEmail();
+                cliente.setEmail(dadoNovo);
+                break;
+
+            case "senha":
+                dadoAntigo = cliente.getSenha();
+                cliente.setSenha(dadoNovo);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Campo '" + dado + "' não é válido para atualização. Use 'email' ou 'senha'");
+        }
+        clienteRepository.save(cliente);
+        return dado + " alterado de: " + dadoAntigo + " para: " + dadoNovo;
     }
 }

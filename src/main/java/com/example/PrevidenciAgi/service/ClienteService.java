8cc126a0d7418d93c11af5ac6540cea7cda7e58f
rafaeldossanto <rafael.santoss2802@gmail.com>
2025-09-23 03:39:02 -0300

    .

--- src/main/java/com/example/PrevidenciAgi/service/ClienteService.java
+++ src/main/java/com/example/PrevidenciAgi/service/ClienteService.java
@@ -12,7 +12,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.List;
 
 
